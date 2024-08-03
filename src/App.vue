86baf69caeaa0e707ef83a9428bb9e894bacfcd4
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-08-04 00:29:13 +0200

    fixed up the colors for the categories and news section

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,38 @@
       <img src="" alt="notification">
     </div>
   </div>
-  <main class="flex justify-between ">
-    <div class="categories w-[20%] h-[88vh] bg-red-200 pl-[30px]">
-      <div class="h-[525px] bg-white border rounded-md mt-[120px]">
-        <p class="p-[20px] border text-[18px] border-b-2">Categories</p>
+  <main class="flex justify-between bg-[#f7f3f3] ">
+    <div class="categories w-[20%] h-[88vh] pl-[30px]">
+      <div class="h-[525px] bg-white  rounded-xl mt-[120px]">
+        <p class="p-[20px]  text-[18px] border-b-2 border-black">Categories</p>
       </div>
-      <div class="h-[200px] bg-white border rounded-md mt-[20px]">
-        <p class="p-[20px] border text-[18px] border-b-2">Settings</p>
+      <div class="h-[200px] bg-white  rounded-xl mt-[20px]">
+        <p class="p-[20px]  text-[18px] border-b-2 border-black" >Settings</p>
       </div>
     </div>
-    <div class="newsheadlines  w-[45%] bg-blue-200">
-      <h2 class=" mt-[40px]  text-[25px] font-bold absolute">News category..space">
-        <div class="bg-red-300">
+    <div class="newsheadlines  w-[45%] ">
+      <h2 class=" mt-[40px]  text-[25px] font-bold absolute">News category...</h2>
+      <div class=" mt-[120px]  h-[76vh] relative overflow-auto space-y-12">
+        <div class="bg-white">
+          <img src="" alt="" class="h-[300px] border rounded-xl border-black">
+          <div class="reactions flex w-[200px] justify-between ml-[auto] relative mt-[10px]">
+            <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
+            <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
+            <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
+          </div>
+          <h5 class="mt-[20px] text-[25px] font-bold">Turkey blocks Instagram access in “censorship” row after Hamas leader’s assassination</h5>
+          <p class="author text-[18px] font-serif">R.O Mandipa</p>
+          <p class="mt-[20px] text-[17px]">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime aperiam itaque temporibus vero, exercitationem voluptate assumenda molestias quaerat, ipsa tempora dolorem veritatis ea possimus eaque, voluptates beatae ullam quis totam!
+
+            You'll want to evaluate almost every model you ever build. In most (though not all) applications, the relevant measure of model quality is predictive accuracy. In other words, will the model's predictions be close to what actually happens.
+
+            Many people make a huge mistake when measuring predictive accuracy. They make predictions with their training data and compare those predictions to the target values in the training data. You'll see the problem with this approach and how to solve it in a moment, but let's think about how we'd do this first.
+
+            You'd first need to summarize the model quality into an understandable way. If you compare predicted and actual home values for 10,000 houses, you'll likely find mix of good and bad predictions. Looking through a list of 10,000 predicted and actual values would be pointless. We need to summarize this into a single metric.
+          </p>
+        </div>
+        <div class="">
           <img src="" alt="" class="h-[300px] border rounded-xl border-black">
           <div class="reactions flex w-[200px] justify-between ml-[auto] relative mt-[10px]">
             <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
@@ -29,23 +49,12 @@
           <h5 class="mt-[20px] text-[25px] font-bold">Turkey blocks Instagram access in “censorship” row after Hamas leader’s assassination</h5>
           <p class="author text-[18px] font-serif">R.O Mandipa</p>
           <p class="mt-[20px] ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime aperiam itaque temporibus vero, exercitationem voluptate assumenda molestias quaerat, ipsa tempora dolorem veritatis ea possimus eaque, voluptates beatae ullam quis totam!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime aperiam itaque temporibus vero, exercitationem 
+            voluptate assumenda molestias quaerat, ipsa tempora dolorem veritatis ea possimus eaque, voluptates beatae ullam 
+            quis totam!
           </p>
         </div>
-        <div class="bg-red-300">
-          <img src="" alt="" class="h-[300px] border rounded-xl border-black">
-          <div class="reactions flex w-[200px] justify-between ml-[auto] relative mt-[10px]">
-            <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
-            <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
-            <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
-          </div>
-          <h5 class="mt-[20px] text-[25px] font-bold">Turkey blocks Instagram access in “censorship” row after Hamas leader’s assassination</h5>
-          <p class="author text-[18px] font-serif">R.O Mandipa</p>
-          <p class="mt-[20px] ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime aperiam itaque temporibus vero, exercitationem voluptate assumenda molestias quaerat, ipsa tempora dolorem veritatis ea possimus eaque, voluptates beatae ullam quis totam!
-          </p>
-        </div>
-        <div class="bg-red-300">
+        <div class="">
           <img src="" alt="" class="h-[300px] border rounded-xl border-black">
           <div class="reactions flex w-[200px] justify-between ml-[auto] relative mt-[10px]">
             <div class="h-[50px] w-[50px] rounded-full bg-blue-300"></div>
